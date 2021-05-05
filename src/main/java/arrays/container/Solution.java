3069f7994c027e825858b7d77fde24b256694659
Yuriy Privezentsev <y.privezentsev@gmail.com>
2021-05-05 21:50:18 +0200

    Biggest container solution

--- src/main/java/arrays/container/Solution.java
+++ src/main/java/arrays/container/Solution.java
@@ -1,15 +1,25 @@
 package arrays.container;
 class Solution {
     public int maxArea(int[] height) {
-        int area = 0;
-        for (int i = 0; i < height.length; i++) {
-            int left = height[i];
-            for (int j = height.length - 1; j > i; j--) {
-                int right = height[j];
-                int rectHeight = Math.min(left, right);
-                area = Math.max(area, rectHeight * (j-i));
+        int left = 0, right = height.length -1;
+        int leftPole = height[left], rightPole = height[right];
+        int area = Math.min(leftPole,rightPole) * (right-left);
+        while(left != right ){
+            if(leftPole > rightPole){
+                while(rightPole >= height[right] && right > left){
+                    right--;
+                }
+                rightPole = height[right];
+                area = Math.max(area, Math.min(leftPole,rightPole) * (right-left));
+            } else {
+                while(leftPole >= height[left] && right > left) {
+                    left++;
+                }
+                leftPole = height[left];
+                area = Math.max(area, Math.min(leftPole,rightPole) * (right-left));
             }
         }
+
         return area;
     }
 }
